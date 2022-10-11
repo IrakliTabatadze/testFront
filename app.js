4c80139bdc3f06163462c24eeafcc51fd187c2ca
unknown <irakli.tabatadze.1@btu.edu.ge>
2022-10-11 22:05:59 +0400

    comment

---
+++ app.js
@@ -0,0 +1,328 @@
+const data = [{
+    monday: [
+        {   
+            id: 1,
+            start: 10,
+            end: 20,
+            title: 'ურჩხული და ლამაზმანი',
+            cover: 'https://tkt-static.lemon.do/img/5161dbdb-0050-48f4-b7f2-4c21c8a7ba36.jpeg',
+        },
+        {
+            id: 2,
+            start: 10,
+            end: 20,
+            title: 'მერე რა რომ, სველია სველი იასამანი',
+            cover: 'https://tkt-static.lemon.do/img/0f40afe7-396d-4158-8526-0b56b0c6b255.jpeg',
+        },
+        {
+            id: 3,
+            start: 10,
+            end: 20,
+            title: 'შეყრა',
+            cover: 'https://tkt-static.lemon.do/img/68eaefd8-2a61-48bc-a0ce-7261aab14bc8.jpeg',
+        }
+    ],
+    tuesday: [
+        {
+            id: 4,
+            start: 10,
+            end: 20,
+            title: 'მე, ბებია, ბაბუა და ჭინკები',
+            cover: 'https://tkt-static.lemon.do/img/410c4b0e-d63a-46c3-bb74-d3a03f753853.jpeg',
+        },
+        {
+            id: 5,
+            start: 10,
+            end: 20,
+            title: 'ყვავილები ელჯერნონისთვის',
+            cover: 'https://tkt-static.lemon.do/img/053b9bdf-604e-4080-9b31-07d56e830f6d.jpeg',
+        },
+        {
+            id: 6,
+            start: 10,
+            end: 20,
+            title: 'ფრთხილად ადამსები!',
+            cover: 'https://tkt-static.lemon.do/img/1db992f5-1d32-4dd7-a6a0-3bc668910b21.jpeg',
+        }
+    ],
+    wednesday: [
+        {
+            id: 7,
+            start: 10,
+            end: 20,
+            title: 'სხვების ცხოვრება',
+            cover: 'https://tkt-static.lemon.do/img/01596ec6-7ef8-41dc-84a6-3adb84545d60.jpeg',
+        },
+        {
+            id: 8,
+            start: 10,
+            end: 20,
+            title: 'რამონა',
+            cover: 'https://tkt-static.lemon.do/img/52247154-7cb1-495f-979c-013e2730e59c.jpeg',
+        },
+        {
+            id: 9,
+            start: 10,
+            end: 20,
+            title: 'კომედიანტები',
+            cover: 'https://tkt-static.lemon.do/img/c0e68ee5-b119-476e-81f1-1bb33589e776.jpeg',
+        }
+    ],
+    thursday: [
+        {
+            id: 10,
+            start: 10,
+            end: 20,
+            title: 'ჩემი გაზაფხულის შემოდგომა',
+            cover: 'https://tkt-static.lemon.do/img/94b6b77e-ca60-4b1a-b24a-3eb26092403d.jpeg',
+        },
+        {
+            id: 11,
+            start: 10,
+            end: 20,
+            title: 'ანუ მე შენ მიყვარხარ',
+            cover: 'https://tkt-static.lemon.do/img/c19054cd-fa85-4924-9895-0ff6597dbfb5.jpeg',
+        },
+        {
+            id: 12,
+            start: 10,
+            end: 20,
+            title: 'მოხუცი და ზღვა',
+            cover: 'https://tkt-static.lemon.do/img/2b4be8eb-c6be-4ef7-b4fb-c3933a45f123.jpeg',
+        }
+    ],
+    friday: [
+        {
+            id: 13,
+            start: 10,
+            end: 20,
+            title: 'ყველაზე მაგარი გოგოები',
+            cover: 'https://tkt-static.lemon.do/img/a81df611-4dbd-4a91-8002-83abaeadd65d.jpeg',
+        },
+        {
+            id: 14,
+            start: 10,
+            end: 20,
+            title: 'ალადინი',
+            cover: 'https://tkt-static.lemon.do/img/0d97441c-8656-49cf-bc49-f0c951a9e39b.jpeg',
+        },
+        {
+            id: 15,
+            start: 10,
+            end: 20,
+            title: 'ურჩხული და ლამაზმანი',
+            cover: 'https://tkt-static.lemon.do/img/5161dbdb-0050-48f4-b7f2-4c21c8a7ba36.jpeg',
+        }
+    ],
+    saturday: [
+        {
+            id: 16,
+            start: 12,
+            end: 22,
+            title: 'წითელქუდა',
+            cover: 'https://tkt-static.lemon.do/img/9a0ced6e-329b-4b6b-951e-1748c8621ee2.jpeg',
+        },
+        {
+            id: 17,
+            start: 12,
+            end: 22,
+            title: 'გაყრა',
+            cover: 'https://tkt-static.lemon.do/img/b6e3f2c1-9cce-4fd4-8d42-4e71ffafb1a1.jpeg',
+        },
+        {
+            id: 18,
+            start: 12,
+            end: 22,
+            title: 'ზღაპარი იგი მათრობს და მხიბლავს',
+            cover: 'https://tkt-static.lemon.do/img/f35816df-43b2-43ce-a844-3c89f82c439f.jpeg',
+        }
+    ],
+    sunday: [
+        {
+            id: 19,
+            start: 12,
+            end: 22,
+            title: 'ცილინდრი',
+            cover: 'https://tkt-static.lemon.do/img/82c2e9e4-4ef7-4fe5-a8c9-008425babdf9.jpeg',
+        },
+        {
+            id: 20,
+            start: 12,
+            end: 22,
+            title: 'შვლის ნუკრის ნაამბობი',
+            cover: 'https://tkt-static.lemon.do/img/f7ea467f-b4b4-42f8-9a0a-9fd0e9f7618f.jpeg',
+        },
+        {
+            id: 21,
+            start: 12,
+            end: 22,
+            title: 'წითელი ფეხსაცმელები',
+            cover: 'https://tkt-static.lemon.do/img/228702ef-7486-4160-8b3e-d1dd60e50993.jpeg',
+        }
+    ],
+}];
+
+//Preparing info for localStorage
+let sessionDay;
+let movieId;
+let sessionTimes = [];
+
+
+function dateHandler(e){
+    // Array of Week days
+    const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    // Array of weekend days
+    const weekEnd = ["Saturday", "Sunday"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dateString = e.target.value;
+    // Get day(string) from input value
+    let day = days[new Date(dateString).getDay()];
+
+    // Checks if selected input(date) is weekday or not and sets relevant time for film sessions , 10:00 - 18:00 for Weekdays and 12:00 - 20:00 for Weekend(Saturday, Sunday)
+    if(weekDays.includes(day)) {
+        let values = ["10:00","12:00","14:00","16:00","18:00"];
+
+        //Add comment!!
+        let movieBoxDiv = document.querySelector(".movies-list");
+        let movieBoxDivChild = movieBoxDiv.lastElementChild;
+        if(movieBoxDivChild != "null"){
+            while(movieBoxDiv.firstChild){
+                movieBoxDiv.removeChild(movieBoxDiv.lastChild)
+            }
+        }
+        renderMovieCard(data, day.toLowerCase());
+        for(const val of values) {
+            for(let i=0; i<document.getElementsByClassName("btn").length; i++){
+                document.getElementsByClassName("btn")[i].innerHTML += `
+                <button type="button" id="res-btn" class="reserve-button">${val}</button>
+                `
+            }
+        }
+        
+        let reserveButtons = document.getElementsByClassName("reserve-button");
+        for(const reserveButton of reserveButtons){
+            reserveButton.addEventListener("click", function onClick(){
+                if(reserveButton.style.background == "green"){
+                    reserveButton.style.background = "#efefef"
+                } else {
+                    reserveButton.style.background = "green";
+                    // console.log(reserveButton.innerHTML);
+                }
+    })
+}
+    } else {
+        let values = ["12:00","14:00","16:00","18:00","20:00"];
+
+        //Add comment!!
+        let movieBoxDiv = document.querySelector(".movies-list");
+        let movieBoxDivChild = movieBoxDiv.lastElementChild;
+        if(movieBoxDivChild != "null"){
+            while(movieBoxDiv.firstChild){
+                movieBoxDiv.removeChild(movieBoxDiv.lastChild)
+            }
+        }
+        renderMovieCard(data, day.toLowerCase());
+        for(const val of values) {
+            for(let i=0; i<document.getElementsByClassName("btn").length; i++){
+                document.getElementsByClassName("btn")[i].innerHTML += `
+                <button type="button" id="res-btn" class="reserve-button">${val}</button>
+                `
+            }
+        }
+        // Reserve button addEventListener
+        let reserveButtons = document.getElementsByClassName("reserve-button");
+        for(const reserveButton of reserveButtons){
+            reserveButton.addEventListener("click", function onClick(){
+                if(reserveButton.style.background == "green"){
+                    reserveButton.style.background = "#efefef"
+                } else {
+                    reserveButton.style.background = "green";
+                }
+            })
+        }
+    }
+    let selectedDay = day.toLowerCase();
+    sessionDay = selectedDay;
+}
+
+function buttonHandler(clicked_id){
+    //id of the movie
+    let idOfMovie = clicked_id.split("-")[1];
+    movieId = idOfMovie;
+    // now I want to check if any of the session time is clicked, if clicked save it on local storage.
+    let movieDiv = document.getElementById(idOfMovie);
+    let buttonsDiv = movieDiv.querySelectorAll(".btn button");
+
+    for(const oneButton of buttonsDiv){
+        if(oneButton.style.background == "green"){
+            oneButton.style.background = "red";
+            sessionTimes.push(oneButton.innerHTML);
+        }
+    }
+    let movieInfo = {
+        "sessionday": sessionDay,
+        "movieid" : movieId,
+        "sessiontime" : sessionTimes
+    }
+    storeToLocalStorage(movieInfo);
+    getFromLocalStorage(movieInfo);
+}
+
+
+// Dynamically renders movieCard(title, image, id), used in dateHandler function
+function renderMovieCard(obj, day){
+    let div = document.createElement("div");
+    div.className = "movie-card";
+    let img = document.createElement("img");
+    img.className = "movie-img";
+    let h1 = document.createElement("h1");
+    h1.className = "movie-title";
+    div.appendChild(img);
+    div.appendChild(h1);
+
+    let moviesList = document.getElementById("movieslist");
+    
+    obj[0][day].map(oneMovie => {
+        moviesList.innerHTML += `
+        <div class="movie-card" id=${oneMovie.id}>
+            <h1 id="movie-title-id" class="movie-title">${oneMovie.title}</h1>
+            <img class="movie-img" src="${oneMovie.cover}">
+            <div class="btn" id="reserve-buttons"></div>
+            <button id="movie-${oneMovie.id}" type="submit" class="submit-reserve" onclick="buttonHandler(this.id)" id="submit-reserve-btn">Reserve ticket</button>
+        </div>
+        `
+    })
+}
+
+function storeToLocalStorage(obj) {
+    localStorage.setItem(`${obj}`, JSON.stringify(obj));
+ }
+
+function getFromLocalStorage(obj) {
+    let retrievedObject = localStorage.getItem(`${obj}`);
+    console.log(JSON.parse(retrievedObject))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
